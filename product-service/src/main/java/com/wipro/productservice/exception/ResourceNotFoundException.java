5f604082793e3f38936e410fe31f486409080a9b
subbu konatham <162993388+8688233328@users.noreply.github.com>
2024-04-22 16:12:51 +0530

    Delete product-service directory

--- product-service/src/main/java/com/wipro/productservice/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-
-package com.wipro.productservice.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-	public ResourceNotFoundException(String message) {
-		   super(message);
-	   }
-}
