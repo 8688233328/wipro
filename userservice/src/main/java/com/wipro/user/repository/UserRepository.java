849dec98d09d641d8bd8472a93ceed174b4d98a4
subbu konatham <162993388+8688233328@users.noreply.github.com>
2024-04-30 16:52:54 +0530

    Delete userservice directory

--- userservice/src/main/java/com/wipro/user/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wipro.user.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.wipro.user.entity.User;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-
-}
