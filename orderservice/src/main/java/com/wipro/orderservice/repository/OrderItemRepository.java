e72bfb16e440a4868a18ffd0e5c5b88a64b6d7cc
subbu konatham <162993388+8688233328@users.noreply.github.com>
2024-05-04 14:11:23 +0530

    Delete orderservice directory

--- orderservice/src/main/java/com/wipro/orderservice/repository/OrderItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wipro.orderservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.wipro.orderservice.entity.OrderItem;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem,Integer>{
-
-}
