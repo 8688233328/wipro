a0eb73f3dc5bf0e4d9c846160c13215f2428e936
subbu konatham <162993388+8688233328@users.noreply.github.com>
2024-05-04 14:13:48 +0530

    Add files via upload

---
+++ orderservice/src/main/java/com/wipro/orderservice/service/OrderService.java
@@ -0,0 +1,24 @@
+package com.wipro.orderservice.service;
+
+import java.util.List;
+
+import com.wipro.orderservice.entity.Order;
+import com.wipro.orderservice.model.OrderResponse;
+import com.wipro.orderservice.payload.OrderItemPayload;
+
+public interface OrderService {
+	
+	Order createOrder(int userId, List<OrderItemPayload> selectedMedicines);
+
+	Order saveOrder(Order order);
+	
+	OrderResponse getOrderDetails(int orderId);
+	
+	List<OrderResponse> getAllOrders();
+	
+	Order updateOrder(Order order);
+
+	void deleteOrder(int orderId);
+	
+//	List<Order> getAllOrdersByCustomer(int customerId);
+}
